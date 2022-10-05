c199193c01dd73c409331302b9408a9ef75536c3
Harsh Kanojia <96467990+Eyesnatcher-coder@users.noreply.github.com>
2022-10-05 19:05:32 +0530

    Successfully added a Navbar

--- myportfolio/src/component/Navbar.jsx
+++ myportfolio/src/component/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <>
-    <div>Navbar</div>
-    <NavLink to='/'><div className="home">Home</div></NavLink>
-    <NavLink to='/about'><div className="about">About</div></NavLink>    
-    </>
+    <div>
+      <div className='navbar'>
+        <div className='brand'>My Portfolio</div>
+        <div className='options'>
+          <NavLink  className="home" to='/'><div>Home</div></NavLink>
+          <NavLink className="about" to='/about'><div >About</div></NavLink>
+        </div>
+      </div>
+    </div>
   )
 }
 
