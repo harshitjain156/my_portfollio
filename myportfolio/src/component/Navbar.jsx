5e7d6b2d072fa2ae65620ad91f9a775e726c86bb
Shalini yadav <shaliniyadav0203@gmail.com>
2022-10-09 13:45:02 +0530

    navabar updated

--- myportfolio/src/component/Navbar.jsx
+++ myportfolio/src/component/Navbar.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import "./Navbar.css"
 
+
 function Navbar() {
+
   return (
     <div>
-      <div className='navbar'>
+      <div className="navbar">
         <div className='brand'>My Portfolio</div>
         <div className='options'>
           <NavLink  className="home" to='/'><div>Home</div></NavLink>
           <NavLink className="about" to='/about'><div >About</div></NavLink>
+          <NavLink className="about" to='/skills'><div >Skills</div></NavLink>
         </div>
       </div>
     </div>
